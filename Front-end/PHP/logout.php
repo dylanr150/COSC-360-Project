105ba8848d280ff397ac3e3651813dde26af587c
Tyler Posella <Tyler.Posella.Stu@gmail.com>
2024-03-29 15:49:26 -0700

    Added logout button

---
+++ Front-end/PHP/logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+	unset($_SESSION["email"]);
+	unset($_SESSION["username"]);
+	unset($_SESSION["firstname"]);
+	unset($_SESSION["lastname"]);
+	unset($_SESSION["date"]);
+	header("Location: http://localhost/COSC-360-Project/Front-end/HTML/login.html");
+session_destroy();
+?>
